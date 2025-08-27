88b6c2b27342c88289c33dfd15646af4db663461
Nitin Maurya <nitin101ji@gmail.com>
2025-08-27 14:22:22 +0530

    .

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-import { connectDB } from "./db/index.js"; // If you're using named export
+import { connectDB } from "./db/index.js"; 
 dotenv.config({ path: "./.env" });
 
 connectDB();
